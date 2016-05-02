f6cdc1c7c749bf166c194c9ff99b7105a92ff367
Ainhoa <ainholopezugarte@gmail.com>
2016-05-02 21:08:30 +0200

    Added files via upload

---
+++ dist/js/masonry.js
@@ -0,0 +1,18 @@
+var $grid = $('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true	
+})
+
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});
+
+$(function() {
+	$('a[href*=#]').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+	});
+});
